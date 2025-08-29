5f09767e322001ba5a68e5a143e819698debb569
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-08-29 16:28:58 -0300

    Add update user route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post("/", authMiddleware,userController.createUser);
 router.get("/", authMiddleware, userController.getUsers);
+router.put("/", authMiddleware, userController.updateUser);
 
 
 router.post("/login", userController.loginUser)

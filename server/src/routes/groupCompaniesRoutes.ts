3fdccb267c3a98223c73c5a805791890673ddedf
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-08-31 22:38:09 -0300

    Add groupcompany delete route

--- server/src/routes/groupCompaniesRoutes.ts
+++ server/src/routes/groupCompaniesRoutes.ts
@@ -7,6 +7,7 @@ const router = Router();
 router.post("/", authMiddleware, groupController.createGroup);
 router.get("/", authMiddleware, groupController.listGroups);
 router.put("/:id", authMiddleware, groupController.updateGroup);
+router.delete("/:id", authMiddleware, groupController.deleteGroup);
 
 
 export default router;

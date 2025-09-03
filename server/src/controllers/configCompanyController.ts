1880583f2c25887b616f72e9d94b641d5b779464
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-09-03 10:05:22 -0300

    configCompany controller

---
+++ server/src/controllers/configCompanyController.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+import * as configService from '../services/configCompanyServices'
+import { Request, Response } from "express";
+import { handleZodError } from "../utils/handleZodError";
+import { NotFoundError } from "../utils/errors";
+
+const configSchema = z.object({
+  companyId: z.string(),
+  configs: z.object({
+    accountingAccount: z.string().max(10, 'Codigo da conta deve conter no máximo 10 caracteres!'),
+    accountName: z.string(),
+  }).array()
+});
+
+
+export const createConfig = async (req: Request, res: Response) => {
+  try {
+    const parsed = configSchema.parse(req.body);
+    const result = await configService.createConfig(parsed)
+
+    res.json(result);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+        return res.status(400).json({ errors: handleZodError(err) });
+      }
+    if (err instanceof NotFoundError) {
+      return res.status(404).json({ error: err.message });
+    }
+    console.error(err);
+    res.status(500).json({ error: "Erro ao salvar Configuração" });
+  }
+};

f219d518dfb4df95c1fbff5715ba0d22440bc13b
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-09-03 10:51:24 -0300

    Add update config company route

--- server/src/controllers/configCompanyController.ts
+++ server/src/controllers/configCompanyController.ts
@@ -68,3 +68,21 @@ export const listConfigCompany = async (req: Request, res: Response) => {
   }
 };
 
+
+export const updateConfigCompany = async (req: Request, res: Response) => {
+  try {
+    const parsed = configSchema.parse(req.body);
+    const result = await configService.createConfig(parsed)
+
+    res.json(result);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+        return res.status(400).json({ errors: handleZodError(err) });
+      }
+    if (err instanceof NotFoundError) {
+      return res.status(404).json({ error: err.message });
+    }
+    console.error(err);
+    res.status(500).json({ error: "Erro ao editar Configurações" });
+  }
+};
--- server/src/routes/configCompanyRoutes.ts
+++ server/src/routes/configCompanyRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import * as configController from "../controllers/configCompanyController";
-import { authMiddleware, requireAdmin } from "../middlewares/authMiddleware";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/", authMiddleware ,configController.createConfig);
-router.get("/", authMiddleware, requireAdmin,  configController.listConfigs);
-router.get("/:id", authMiddleware, requireAdmin,  configController.listConfigCompany);
-// router.put("/:id", authMiddleware, requireAdmin, groupController.updateGroup);
-// router.delete("/:id", authMiddleware, requireAdmin, groupController.deleteGroup);
+router.get("/", authMiddleware, configController.listConfigs);
+router.get("/:id", authMiddleware, configController.listConfigCompany);
+router.put("/", authMiddleware, configController.updateConfigCompany);
+// router.put("/:id", authMiddleware, roupController.updateGroup);
+// router.delete("/:id", authMiddleware, roupController.deleteGroup);
 
 
 export default router;

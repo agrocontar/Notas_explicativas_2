7c782e8e77a3d2775dcdd7c25b133e29657bf86c
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-10-27 09:44:31 -0300

    Add notas controller

--- server/src/services/notasServices.ts
+++ server/src/services/notasServices.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+import { prisma } from '../prismaClient';
 
 // Types para as notas
 export interface CreateNotaData {

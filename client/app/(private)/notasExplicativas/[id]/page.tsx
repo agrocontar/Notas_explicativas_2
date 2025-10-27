a1b93257a00ee178f731b39b075773884345ca1a
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-10-27 11:19:22 -0300

    notasExplicativas page

---
+++ client/app/(private)/notasExplicativas/[id]/page.tsx
@@ -0,0 +1,18 @@
+
+interface NotasExplicativasPageProps {
+  params: {
+    id: string
+  }
+}
+
+
+export default function NotasExplicativasPage({ params }: NotasExplicativasPageProps) {
+
+  return (
+    <div className="grid">
+      <div className="col-12">
+        
+      </div>
+    </div>
+  )
+}
